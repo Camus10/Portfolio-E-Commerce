1f491ea7dafd76c3748ded585aaf952bab1d21bf
Camus10 <mharishidayat@gmail.com>
2022-05-30 11:21:36 +0700

    add more product and card feature

--- client-side/src/core/Home.js
+++ client-side/src/core/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./APICore";
 import Card from "./Card";
+import Search from "./Search";
 
 
 const Home = () => {
@@ -36,18 +37,27 @@ const Home = () => {
 
   return(
     <Layout title="Home Page" description="Node React E-Commerce App" className="container-fluid">
+      <Search />
+      
       <h2 className="mb-4">New Arrival</h2>
       <div className="row">
-        {/* i for index 
-        {productsByArrival.map((product, i) => (<Card key={i} product={product} />))}
-        */}
+        {/* i for index */}
+        {productsByArrival.map((product, i) => (
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
       </div>
       
       
       <h2 className="mb-4">Best Sellers</h2>
       <div className="row">
         {/* i for index */}
-        {productsBySell.map((product, i) => (<Card key={i} product={product} />))}
+        {productsBySell.map((product, i) => (
+          <div key={i} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
+        ))}
       </div>
     </Layout>
   );
